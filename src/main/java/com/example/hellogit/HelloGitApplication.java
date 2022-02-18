d6307d86e69058e35d0d999fe59f4cab9e454b66
taekgyu <taekgyu0602@gmail.com>
2022-02-18 09:31:55 +0900

    User클래스 옮기기

--- src/main/java/com/example/hellogit/HelloGitApplication.java
+++ src/main/java/com/example/hellogit/HelloGitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloGitApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloGitA    lication.class, args);
+        SpringApplication.run(HelloGitApplication.class, args);
     }
 
 }
